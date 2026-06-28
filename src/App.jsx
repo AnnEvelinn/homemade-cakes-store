c844c7462fe1043fce85c72aecc434347f0d0fcd
Ann Evelin <evelinvincentkattor@gmail.com>
2026-06-29 00:48:35 +0530

    added links to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 
-function App() {
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import CustomOrders from "./pages/CustomOrders";
+import OrderGuide from "./pages/OrderGuide";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
+import { Routes, Route } from "react-router-dom";
+
+function App() {
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/custom-orders" element={<CustomOrders />} />
+        <Route path="/order-guide" element={<OrderGuide />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
 
-export default App;   
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,42 @@
-import "../css/Navbar.css";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import "../css/Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
 
       <div className="logo">
-        <img src={logo} alt="TeesuBakes Logo" />
-        <h2>TeesuBakes</h2>
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="TeesuBakes Logo" />
+          <h2>TeesuBakes</h2>
+        </Link>
       </div>
 
       <ul className="nav-links">
 
         <li>
-          <a href="#">Home</a>
+          <Link to="/">Home</Link>
         </li>
 
         <li>
-          <a href="#">Products</a>
+          <Link to="/products">Products</Link>
         </li>
 
         <li>
-          <a href="#">Custom Orders</a>
+          <Link to="/custom-orders">Custom Orders</Link>
         </li>
 
         <li>
-          <a href="#">Order Guide</a>
+          <Link to="/order-guide">Order Guide</Link>
         </li>
 
         <li>
-          <a href="#">About Us</a>
+          <Link to="/about">About Us</Link>
         </li>
 
         <li>
-          <a href="#">Contact</a>
+          <Link to="/contact">Contact</Link>
         </li>
 
       </ul>
